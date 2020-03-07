7b91e3fe20f4a85ce85a7a45fe8f5de2a0421aad
魏小雨 <6682220aabc@163.com>
2020-03-07 19:20:58 +0800

    update

--- src/main/java/com/example/springsession/config/HttpSessionConfig.java
+++ src/main/java/com/example/springsession/config/HttpSessionConfig.java
@@ -1,9 +1,18 @@
 package com.example.springsession.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 @EnableRedisHttpSession
 public class HttpSessionConfig {
+    // Spring session 浏览器sessionId与服务器不一致解决方案
+    @Bean
+    public DefaultCookieSerializer getDefaultCookieSerializer(){
+        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+        cookieSerializer.setUseBase64Encoding(false);
+        return cookieSerializer;
+    }
 }
